16081f3ba2e4dbb6e42ab3915c24a9dd58f8e7da
sino019 <finalofme@gmail.com>
2025-08-27 15:53:56 +0700

    ❤️

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	// 3. Setup routes
 	routes.SetupExampleRoutes(app)
 
-	// 4. Jalankan server
+	// 4. Jalankan servers
 	fmt.Println("Server running at http://localhost:3000")
 	app.Listen(":3000")
 }
